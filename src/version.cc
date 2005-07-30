c46ce464cbba4ea758e50dd11c74b2f74429f377
Bruno Haible <bruno@clisp.org>
2005-07-30 16:42:13 +0000

    Bump version number to 3.0.2.

--- src/version.cc
+++ src/version.cc
@@ -1,6 +1,6 @@
 /* Current program version number.
 
-   Copyright (C) 1989-1998, 2000, 2002-2003 Free Software Foundation, Inc.
+   Copyright (C) 1989-1998, 2000, 2002-2003, 2005 Free Software Foundation, Inc.
    Written by Douglas C. Schmidt <schmidt@ics.uci.edu>
    and Bruno Haible <bruno@clisp.org>.
 
@@ -25,4 +25,4 @@
 #include "version.h"
 
 /* Current release version. */
-const char *version_string = "3.0.1";
+const char *version_string = "3.0.2";
